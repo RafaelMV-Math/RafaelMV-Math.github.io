74b21a1e2d13d8cc6914d3c8147d5e034c3c0461
RafaelMV-Math <rafamv2017@gmail.com>
2024-08-19 14:23:31 +0200

    v6 wth

--- scripts.js
+++ scripts.js
@@ -21,30 +21,27 @@ document.addEventListener('DOMContentLoaded', () => {
 document.addEventListener('DOMContentLoaded', () => {
     const apiKey = '9248181116c7db072f161882d1f945a0'; // Reemplaza con tu clave API de OpenWeatherMap
     const city = 'Barcelona';
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=en`;
+    const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${apiKey}&lang=en`;
 
     fetch(apiUrl)
         .then(response => {
             if (!response.ok) {
-                throw new Error('Error en la respuesta de la API');
+                throw new Error('API response error');
             }
             return response.json();
         })
         .then(data => {
-            const temp = data.main.temp;
-            const weatherDescription = data.weather[0].description;
-            const weatherIcon = data.weather[0].icon;
+            const temp = data.list[0].main.temp;
+            const weatherDescription = data.list[0].weather[0].description;
+            const weatherIcon = data.list[0].weather[0].icon;
             const weatherElement = document.getElementById('weather-info');
 
-            // Construir la URL del icono
             const iconUrl = `http://openweathermap.org/img/wn/${weatherIcon}@2x.png`;
-
-            // Mostrar la información del clima con el icono
-            weatherElement.innerHTML = `<img src="${iconUrl}" alt="Weather icon"> ${temp} C, ${weatherDescription}.`;
+            weatherElement.innerHTML = `<img src="${iconUrl}" alt="Weather icon"> ${temp} C, ${weatherDescription}`;
         })
         .catch(error => {
-            console.error('Error al obtener el clima:', error);
+            console.error('Error fetching weather data:', error);
             const weatherElement = document.getElementById('weather-info');
-            weatherElement.textContent = 'Unable to load the weather.';
+            weatherElement.textContent = 'Unable to load the weather';
         });
 });
